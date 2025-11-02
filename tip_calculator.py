4112e634324cb3a7fa42277af5bca2d64e809ea2
nikitaxzol <nikitaxzol13@gmail.com>
2025-11-02 17:08:28 +0100

    Update: int staff, allow 0% tips, exit on invalid, round to 2

--- tip_calculator.py
+++ tip_calculator.py
@@ -3,7 +3,7 @@ print("=== Tips counter ===")
 try:
     bill = float(input("Write bill amount: "))
     tip_percent = float(input("Write Tips %: "))
-    staff_amount = float(input("Write number of workers:"))
+    staff_amount = int(input("Write number of workers:"))
 except ValueError:
     print("Only numbers, sorry, try again")
     exit()
@@ -11,19 +11,20 @@ except ValueError:
 if bill <= 0 : 
     print("sorry it can't be less that 0, try again please")
     exit()
-if tip_percent <= 0 : 
+if tip_percent < 0 : 
     print("sorry it can't be less that 0, try again please")
     exit()
 if staff_amount <= 0 :
     print("sorry it can't be less that 0, try again please")
+    exit()
 
 tip = bill * tip_percent/100
 total = bill + tip
 tip_per_person = tip / staff_amount
 
 
-tip = round (tip, 5)
-total = round (total, 5)
+tip = round (tip, 2)
+total = round (total, 2)
 tip_per_person = round (tip_per_person, 1)
 
 print("Tips:", tip)
